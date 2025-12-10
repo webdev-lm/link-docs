d7d0054c77bafc62dbd585d99e709c363d694df8
Jan Schuster <webdev@linkmobility.com>
2025-12-10 19:14:10 +0100

    npmrc & blob

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {}
 };
 
-export default withPayload(withPayload(nextConfig));
+export default withPayload(nextConfig);
