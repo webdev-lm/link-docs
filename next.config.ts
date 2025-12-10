a2fcbc25888cdfa7e4bad510f1119fc82cef67cb
Jan Schuster <webdev@linkmobility.com>
2025-12-10 19:45:09 +0100

    hotfix for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,8 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   /* config options here */
 };
 
-const payloadConfig = withPayload(nextConfig)
-
-payloadConfig.turbopack = undefined
-export default payloadConfig
+export default withPayload(nextConfig);
